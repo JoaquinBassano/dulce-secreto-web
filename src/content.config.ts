d7b34e795a7ef958e233194162d875624bc1f66b
Joaquin Bassano <joaquinbas98@gmail.com>
2025-12-18 00:47:25 -0300

    feat: add landing footer and remove unused components

--- src/content.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
-
-// Define collections here when needed
-// const blog = defineCollection({ ... });
-
-export const collections = {};
